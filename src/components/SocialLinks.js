c808fa2f617f235e9976c2b1da40f06ea2347b92
Narmada2001 <155349750+Narmada2001@users.noreply.github.com>
2025-10-27 11:28:03 +0530

    Update SocialLinks.js

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -6,10 +6,10 @@ import './SocialLinks.css'; // Create this CSS file
 const SocialLinks = () => {
   return (
     <div className="social-links">
-      <a href="https://www.linkedin.com/in/narmada-gunathilaka/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/in/narmada-gunathilaka-2818372a7/" target="_blank" rel="noopener noreferrer">
         <FaLinkedin />
       </a>
-      <a href="https://github.com/your-github-profile" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/Narmada2001" target="_blank" rel="noopener noreferrer">
         <FaGithub />
       </a>
       <a href="mailto:narmadagunathilaka9@gmail.com">
